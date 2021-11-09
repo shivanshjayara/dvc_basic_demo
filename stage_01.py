2616b4d7ce2a07f6e9c2fa2dcb3b6f73d8004c8b
s4shivansh29 <s4shivansh29@gmail.com>
2021-11-09 22:04:17 +0530

    first commit

---
+++ stage_01.py
@@ -0,0 +1,5 @@
+text="input 01"
+
+with open('artificates01.txt','w') as f:
+    f.write(text)
+    #hlk
---
+++ stage_02.py
@@ -0,0 +1,5 @@
+with open('artificates01.txt','r') as f:
+    text=f.read()
+print(text)
+
+#this file is to read the text file
